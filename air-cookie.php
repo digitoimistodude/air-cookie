6a29eebabe3b6284b84c1b5f3e89ac47a67529bd
Timi Wahalahti <timi@wahalahti.fi>
2021-09-08 10:22:22 +0300

    plugin headers

--- air-cookie.php
+++ air-cookie.php
@@ -1,12 +1,20 @@
 <?php
 /**
  * Plugin Name: Air Cookie
+ * Plugin URI: https://github.com/digitoimistodude/air-cookie
+ * Description: Simple cookie banner and management.
  * Version: 0.1.0
+ * Author: Digitoimisto Dude Oy, Timi Wahalahti
+ * Author URI: https://www.dude.fi
+ * Requires at least: 5.5
+ * Tested up to: 5.8
+ * License: GPL-3.0+
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  *
  * @Author: Timi Wahalahti
  * @Date:   2021-08-10 10:49:07
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-09-07 17:23:02
+ * @Last Modified time: 2021-09-08 10:19:06
  * @package air-cookie
  */
 
@@ -33,7 +41,7 @@ function get_plugin_version() {
  * @since 0.1.0
  */
 function get_databse_version() {
-  return 20210824; // date without dashes
+  return 20210907; // date without dashes
 } // end get_databse_version
 
 /**
@@ -52,17 +60,42 @@ function get_script_version() {
 */
 require 'plugin-helpers.php';
 
+/**
+ * Compile settings for the script.
+ *
+ * @since 0.1.0
+ */
 require plugin_base_path() . '/settings.php';
 
+/**
+ * Default strings and translation support for those.
+ *
+ * @since 0.1.0
+ */
 require plugin_base_path(). '/strings.php';
 add_action( 'init', __NAMESPACE__ . '\register_strings' );
 
+/**
+ * The actual script and javascript related things.
+ *
+ * @since 0.1.0
+ */
 require plugin_base_path() . '/script-injection.php';
 add_action( 'wp_head', __NAMESPACE__ . '\inject_js' );
 
+/**
+ * Rest api for recording the visitor consents.
+ *
+ * @since 0.1.0
+ */
 require plugin_base_path() . '/rest-api.php';
 add_action( 'rest_api_init', __NAMESPACE__ . '\register_rest_endpoint' );
 
+/**
+ * Databse creation.
+ *
+ * @since 0.1.0
+ */
 require plugin_base_path(). '/database.php';
 add_action( 'admin_init', __NAMESPACE__ . '\maybe_init_database' );
 
